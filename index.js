e9c1f66cfb24d23651de4745cb37989270356707
Mr-Hypocrite <ym.jatin@gmail.com>
2022-11-21 21:34:35 +0530

    chore: eslintrc config

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const axios = require("axios");
-const express = require("express");
-const cheerio = require("cheerio");
+const axios = require( "axios" );
+const express = require( "express" );
+const cheerio = require( "cheerio" );
 const PORT = 5000;
 
 const url = "https://www.amazon.in/dp/B0BCP3X81V/";
 
 const app = express();
 
-const apiCall = async (url) => {
-  const response = await axios(url);
-  const html = response.data;
-  const $ = cheerio.load(html);
-  $("#productTitle", html).each(function () {
-    const title = $(this).text();
-    console.log(title);
-  });
+const apiCall = async ( url ) => {
+    const response = await axios( url );
+    const html = response.data;
+    const $ = cheerio.load( html );
+    $( "#productTitle", html ).each( function () {
+        const title = $( this ).text();
+        console.log( title );
+    } );
 
-  // $(".a-price-whole", html).each(function () {
-  //   const price = $(this).text();
-  //   console.log(price);
-  // });
+    // $(".a-price-whole", html).each(function () {
+    //   const price = $(this).text();
+    //   console.log(price);
+    // });
 
-  // $(".a-list-item", html).each(function () {
-  //   const list = $(this).text();
-  //   console.log(list);
-  // });
+    // $(".a-list-item", html).each(function () {
+    //   const list = $(this).text();
+    //   console.log(list);
+    // });
 };
 
-apiCall(url);
+apiCall( url );
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen( PORT, () => console.log( `listening on port ${PORT}` ) );
